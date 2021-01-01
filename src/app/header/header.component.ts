255923bcc90e204f3fdaa7a30c21ed6115a69d3b
Weverton procópio <wevertonprocopio690@gmail.com>
2021-01-01 11:37:42 -0300

    test design header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,51 +19,51 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const navLinks = document.querySelectorAll('.nav-link');
-    const tab = document.querySelector('.tab');
-    const convertTab = tab as HTMLElement;
+    // const navLinks = document.querySelectorAll('.nav-link');
+    // const tab = document.querySelector('.tab');
+    // const convertTab = tab as HTMLElement;
 
-    const navLink = navLinks[0] as HTMLElement;
-    const localHasClick = localStorage.getItem('hasClick');
+    // const navLink = navLinks[0] as HTMLElement;
+    // const localHasClick = localStorage.getItem('hasClick');
 
-    if (localHasClick === 'no click' || localHasClick === null) {
-      setTimeout(() => this.moveTab(navLink, convertTab), 0);
-    } else {
-      setTimeout(() => this.handleClick(navLinks, convertTab), 0);
-    }
+    // if (localHasClick === 'no click' || localHasClick === null) {
+    //   setTimeout(() => this.moveTab(navLink, convertTab), 0);
+    // } else {
+    //   setTimeout(() => this.handleClick(navLinks, convertTab), 0);
+    // }
 
-    navLinks.forEach((navLink) =>
-      navLink.addEventListener('click', (event) =>
-        this.onClick(event, convertTab)
-      )
-    );
-  }
-  private onClick(event: Event, convertTab: HTMLElement) {
-    const eventTarget = event.target as HTMLElement;
-    this.hasClick = 'hasClick';
-    localStorage.setItem('hasClick', this.hasClick);
+    // navLinks.forEach((navLink) =>
+    //   navLink.addEventListener('click', (event) =>
+    //     this.onClick(event, convertTab)
+    //   )
+    // );
+  // }
+  // private onClick(event: Event, convertTab: HTMLElement) {
+  //   const eventTarget = event.target as HTMLElement;
+  //   this.hasClick = 'hasClick';
+  //   localStorage.setItem('hasClick', this.hasClick);
 
-    const navLink = eventTarget;
-    console.log('setId', localStorage.setItem('id', eventTarget.id));
+  //   const navLink = eventTarget;
+  //   console.log('setId', localStorage.setItem('id', eventTarget.id));
 
-    this.moveTab(navLink, convertTab);
-  }
+  //   this.moveTab(navLink, convertTab);
+  // }
 
-  private moveTab(navLink: HTMLElement, tab: HTMLElement): void {
-    const width = navLink.offsetWidth.toString() + 'px';
-    const left = navLink.offsetLeft.toString() + 'px';
+  // private moveTab(navLink: HTMLElement, tab: HTMLElement): void {
+  //   const width = navLink.offsetWidth.toString() + 'px';
+  //   const left = navLink.offsetLeft.toString() + 'px';
 
-    tab.style.width = width;
-    tab.style.left = left;
-  }
-  private handleClick(navLinks: NodeListOf<Element>, convertTab: HTMLElement) {
-    const id = localStorage.getItem('id');
+  //   tab.style.width = width;
+  //   tab.style.left = left;
+  // }
+  // private handleClick(navLinks: NodeListOf<Element>, convertTab: HTMLElement) {
+  //   const id = localStorage.getItem('id');
 
-    navLinks.forEach((navLink) => {
-      const convertNavLink = navLink as HTMLElement;
-      if (id === navLink.id) {
-        this.moveTab(convertNavLink, convertTab);
-      }
-    });
+  //   navLinks.forEach((navLink) => {
+  //     const convertNavLink = navLink as HTMLElement;
+  //     if (id === navLink.id) {
+  //       this.moveTab(convertNavLink, convertTab);
+  //     }
+  //   });
   }
 }
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HeaderComponent } from './header.component';
 import { DialogModule } from './dialog/dialog.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule, DialogModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+
+    DialogModule,
+  ],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
